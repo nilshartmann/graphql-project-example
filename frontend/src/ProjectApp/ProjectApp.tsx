3cf2af16d99efb77e3bb3a8af42dd701f1fe8f8b
Nils Hartmann <nils@nilshartmann.net>
2019-04-05 14:01:58 +0200

    Add Task Page

--- frontend/src/ProjectApp/ProjectApp.tsx
+++ frontend/src/ProjectApp/ProjectApp.tsx
@@ -1,10 +1,6 @@
 import * as React from "react";
 import * as styles from "./ProjectApp.scss";
-import TasksPage from "../TasksPage";
-import ProjectsPage from "../ProjectsPage";
-import {Form, Input, Select, TextArea} from "../components/form";
-import ButtonBar from "../components/ButtonBar";
-import Button from "../components/Button";
+import AddTaskPage from "../AddTaskPage/AddTaskPage";
 
 function Header() {
 	return <header className={styles.Header}>
@@ -12,41 +8,6 @@ function Header() {
 	</header>
 }
 
-function AddTaskPage() {
-	return <div className={styles.AddTaskPage}>
-		<header>
-		<h1>Frühjahrsputz - Add new Task</h1>
-		</header>
-		<Form>
-			<Input label="Name"/>
-			<Input label="Finish until"/>
-			<Select label="Assign to"/>
-			<TextArea label="Description"/>
-
-			<ButtonBar>
-				<Button secondary>Cancel</Button>
-				<Button>Save</Button>
-			</ButtonBar>
-
-		</Form>
-
-	</div>
-}
-
-
-{/*<input type="text" value={"Klaus"} />*/}
-{/*<label>Finish until</label>*/}
-{/*<input type="text" value={"12.03.2019"} />*/}
-{/*<label>Assign To</label>*/}
-{/*<select>*/}
-{/*<option value={"klaus"}>Klaus</option>*/}
-{/*	<option value={"maja"}>Maja</option>*/}
-{/*	<option value={"susi"}>Susi</option>*/}
-{/*	<option value={"georg"}>Georg</option>*/}
-{/*	</select>*/}
-
-{/*	<label>Description</label>*/}
-{/*	<textarea rows={5} />*/}
 
 export default function ProjectApp() {
 	return (<div className={styles.ProjectApp}><Header/>
